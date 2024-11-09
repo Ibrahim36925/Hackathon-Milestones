80d4bf4b42130d4fc6d16952c6c40d9ed608540b
Ibrahim Ahmed <105818823+Ibrahim36925@users.noreply.github.com>
2024-11-10 00:09:42 +0500

    Update script.js

--- Milestone-3/script.js
+++ Milestone-3/script.js
@@ -1,6 +1,5 @@
 var form = document.getElementById('form');
 var resumeContent = document.getElementById('resume-content');
-// Function to handle form submission
 form.addEventListener('submit', function (event) {
     event.preventDefault(); 
     // Retrieve values from the form
@@ -10,7 +9,6 @@ form.addEventListener('submit', function (event) {
     var education = document.getElementById('education').value;
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value.split(',');
-    // Create the resume content
     resumeContent.innerHTML = "\n        <h3>Personal Information</h3>\n        <p>Name:".concat(name, "</p>\n        <p>Email: ").concat(email, "</p>\n        <p>Phone: ").concat(phone, "</p>\n        <h3>Education</h3>\n        <p>").concat(education, "</p>\n        <h3>Work Experience</h3>\n        <p>").concat(experience, "</p>\n        <h3>Skills</h3>\n        <ul>").concat(skills.map(function (skill) { return "<li>".concat(skill.trim(), "</li>"); }).join(''), "</ul>\n    ");
 
     var profilePicInput = document.getElementById('profile-pic');
